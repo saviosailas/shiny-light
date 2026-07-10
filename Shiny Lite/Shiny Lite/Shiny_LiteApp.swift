b0a14eb6acbbbcb1ba04624e3deb3d92490d969c
user <user@example.local>
2026-07-10 22:43:20 +0530

    created app

---
+++ Shiny Lite/Shiny Lite/Shiny_LiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shiny_LiteApp.swift
+//  Shiny Lite
+//
+//  Created by user on 10/07/26.
+//
+
+import SwiftUI
+
+@main
+struct Shiny_LiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shiny Lite/Shiny LiteTests/Shiny_LiteTests.swift
@@ -0,0 +1,19 @@
+//
+//  Shiny_LiteTests.swift
+//  Shiny LiteTests
+//
+//  Created by user on 10/07/26.
+//
+
+import Testing
+@testable import Shiny_Lite
+
+struct Shiny_LiteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
