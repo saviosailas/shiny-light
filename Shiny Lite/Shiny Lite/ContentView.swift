e146b6088e3cc4d53abba2089bde26acd3a25154
user <user@example.local>
2026-07-10 22:57:32 +0530

    used shinyUI on core app

--- Shiny Lite/Shiny Lite/ContentView.swift
+++ Shiny Lite/Shiny Lite/ContentView.swift
@@ -5,17 +5,12 @@
 //  Created by user on 10/07/26.
 //
 
+import ShinyUI
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ShinyUIView()
     }
 }
 
