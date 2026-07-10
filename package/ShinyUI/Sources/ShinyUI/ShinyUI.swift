2efb4410a19b946206cda1cbed1974add2202cdb
user <user@example.local>
2026-07-10 22:50:45 +0530

    shiny ui

---
+++ package/ShinyUI/Sources/ShinyUI/ShinyUI.swift
@@ -0,0 +1,18 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("This is shinny UI")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
